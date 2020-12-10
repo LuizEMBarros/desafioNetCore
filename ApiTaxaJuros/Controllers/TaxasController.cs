503cc6f3a04c3be3e4f042508763e594fe35a62a
LuizEMBarros <luizelias@hotmail.com>
2020-12-10 15:35:45 -0300

    Add files via upload

---
+++ ApiTaxaJuros/Controllers/TaxasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiTaxaJuros.Controllers
+{
+
+    [Route("api/taxaJuros")]
+    public class TaxasController : ControllerBase
+    {
+        private static readonly double taxaPadrao = 0.01;
+        // GET: api/<TaxasController>
+        [HttpGet]
+        public double Get()
+        {
+            return taxaPadrao;
+        }
+     
+    }
+}
